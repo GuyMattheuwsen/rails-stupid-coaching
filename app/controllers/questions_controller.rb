2df42441d657671aeac41d0ba587dcf616a3814b
GuyMattheuwsen <guy.mattheuwsen@gmail.com>
2022-02-14 14:50:49 +0100

    ok

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @query = params[:query]
+    @answer = ""
+    if @query == 'I am going to work'
+      @answer = "Great !"
+    elsif @query.last == "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
